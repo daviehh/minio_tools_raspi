a0e75fa331e1ed334138be3bc77b8ed4deddd38c
daviehh <25255906+daviehh@users.noreply.github.com>
2019-10-10 22:53:36 -0400

    Update build_minio.sh

--- build_minio.sh
+++ build_minio.sh
@@ -5,6 +5,6 @@ latesttag=$(git describe --tags --abbrev=0)
 echo checking out ${latesttag}
 git checkout ${latesttag}
 
-sed -i '1 i\GOARM := $(shell go env GOARM)' Makefile
+sed -i '5 i\GOARM := $(shell go env GOARM)' Makefile
 
 env GOOS=linux GOARCH=arm GOARM=7 make
