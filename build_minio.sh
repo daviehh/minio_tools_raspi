8e157c8c627ab635789d38de960c8fe93397f26a
daviehh <25255906+daviehh@users.noreply.github.com>
2019-10-11 21:27:09 -0400

    path fix

--- build_minio.sh
+++ build_minio.sh
@@ -8,6 +8,6 @@ git checkout ${latesttag}
 go build buildscripts/gen-ldflags.go
 
 sed -i '5 i\GOARM := $(shell go env GOARM)' Makefile
-sed -i 's/^LDFLAGS := .*$/LDFLAGS := $(shell gen-ldflags)/' Makefile
+sed -i 's/^LDFLAGS := .*$/LDFLAGS := $(shell $(PWD)\/gen-ldflags)/' Makefile
 
 env GOOS=linux GOARCH=arm GOARM=7 make
